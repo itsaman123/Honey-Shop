72a02b79f8e4931ef38c3a62f12aea1e2223c12c
Aman Murya <m.amanmaurya49@gmail.com>
2023-07-04 13:04:26 +0530

    added responsive navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,153 +1,253 @@
-import React from 'react';
+// import React from 'react';
+
+// const Navbar = () => {
+//   return (
+//     <>
+//       <nav class="bg-white sticky top-0 border-gray-200 dark:bg-black">
+//         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+//           <a href="https://flowbite.com/" class="flex items-center">
+//             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+//               Honey<span className="text-yellow-600">Kalash</span>
+//             </span>
+//           </a>
+//           <div class="flex items-center md:order-2">
+//             <button
+//               type="button"
+//               class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+//               id="user-menu-button"
+//               aria-expanded="false"
+//               data-dropdown-toggle="user-dropdown"
+//               data-dropdown-placement="bottom"
+//             >
+//               <span class="sr-only">Open user menu</span>
+//               <img
+//                 class="w-8 h-8 rounded-full"
+//                 src="https://assets.leetcode.com/users/avatars/avatar_1676381272.png"
+//                 alt="ima"
+//               />
+//             </button>
+
+//             <div
+//               class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+//               id="user-dropdown"
+//             >
+//               <div class="px-4 py-3">
+//                 <span class="block text-sm text-gray-900 dark:text-white">
+//                   Bonnie Green
+//                 </span>
+//                 <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">
+//                   name@flowbite.com
+//                 </span>
+//               </div>
+//               <ul class="py-2" aria-labelledby="user-menu-button">
+//                 <li>
+//                   <a
+//                     href="/"
+//                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+//                   >
+//                     Dashboard
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a
+//                     href="/"
+//                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+//                   >
+//                     Settings
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a
+//                     href="/"
+//                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+//                   >
+//                     Earnings
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a
+//                     href="/"
+//                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+//                   >
+//                     Sign out
+//                   </a>
+//                 </li>
+//               </ul>
+//             </div>
+//             <button
+//               data-collapse-toggle="mobile-menu-2"
+//               type="button"
+//               class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+//               aria-controls="mobile-menu-2"
+//               aria-expanded="false"
+//             >
+//               <span class="sr-only">Open main menu</span>
+//               <svg
+//                 class="w-6 h-6"
+//                 aria-hidden="true"
+//                 fill="currentColor"
+//                 viewBox="0 0 20 20"
+//                 xmlns="http://www.w3.org/2000/svg"
+//               >
+//                 <path
+//                   fill-rule="evenodd"
+//                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+//                   clip-rule="evenodd"
+//                 ></path>
+//               </svg>
+//             </button>
+//           </div>
+//           <div
+//             class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+//             id="mobile-menu-2"
+//           >
+//             <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+//               <li>
+//                 <a
+//                   href="/"
+//                   class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+//                   aria-current="page"
+//                 >
+//                   Home
+//                 </a>
+//               </li>
+//               <li>
+//                 <a
+//                   href="/"
+//                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+//                 >
+//                   About
+//                 </a>
+//               </li>
+//               <li>
+//                 <a
+//                   href="/"
+//                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+//                 >
+//                   Services
+//                 </a>
+//               </li>
+//               <li>
+//                 <a
+//                   href="/"
+//                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+//                 >
+//                   Pricing
+//                 </a>
+//               </li>
+//               <li>
+//                 <a
+//                   href="/"
+//                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+//                 >
+//                   Contact
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+//     </>
+//   );
+// };
+
+// export default Navbar;
+
+
+
+
+
+
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  return (
-    <>
-      <nav class="bg-white sticky top-0 border-gray-200 dark:bg-black">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" class="flex items-center">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Honey<span className="text-yellow-600">Kalash</span>
-            </span>
-          </a>
-          <div class="flex items-center md:order-2">
-            <button
-              type="button"
-              class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-              id="user-menu-button"
-              aria-expanded="false"
-              data-dropdown-toggle="user-dropdown"
-              data-dropdown-placement="bottom"
-            >
-              <span class="sr-only">Open user menu</span>
-              <img
-                class="w-8 h-8 rounded-full"
-                src="https://assets.leetcode.com/users/avatars/avatar_1676381272.png"
-                alt="ima"
-              />
-            </button>
+  const [isOpen, setIsOpen] = useState(false);
 
-            <div
-              class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-              id="user-dropdown"
-            >
-              <div class="px-4 py-3">
-                <span class="block text-sm text-gray-900 dark:text-white">
-                  Bonnie Green
-                </span>
-                <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">
-                  name@flowbite.com
-                </span>
-              </div>
-              <ul class="py-2" aria-labelledby="user-menu-button">
-                <li>
-                  <a
-                    href="/"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Dashboard
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Earnings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Sign out
-                  </a>
-                </li>
-              </ul>
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <h1 className="text-white font-bold text-lg">Honey<span className="text-yellow-600">Kalash</span></h1>
             </div>
-            <button
-              data-collapse-toggle="mobile-menu-2"
-              type="button"
-              class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
-            >
-              <span class="sr-only">Open main menu</span>
-              <svg
-                class="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div
-            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            id="mobile-menu-2"
-          >
-            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
                 <a
                   href="/"
-                  class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                  aria-current="page"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Home
                 </a>
-              </li>
-              <li>
                 <a
                   href="/"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Products
+                </a>
+                <a
+                  href="/"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   About
                 </a>
-              </li>
-              <li>
                 <a
                   href="/"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Contact
                 </a>
-              </li>
-            </ul>
+              </div>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={handleToggle}
+              className="text-gray-400 hover:text-white inline-flex items-center justify-center p-2 rounded-md focus:outline-none"
+            >
+              {isOpen ? (
+                <FaTimes className="block h-6 w-6" />
+              ) : (
+                <FaBars className="block h-6 w-6" />
+              )}
+            </button>
           </div>
         </div>
-      </nav>
-    </>
+      </div>
+      <div className={isOpen ? "block" : "hidden md:hidden"}>
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <a
+            href="/"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Home
+          </a>
+          <a
+            href="/"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            About
+          </a>
+          <a
+            href="/"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Services
+          </a>
+          <a
+            href="/"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 };
 
